bde6b19e0aaf05d06679aee2cf199579de404bb4
Miriam Sanders <miriamsanders2005@gmail.com>
2025-11-04 12:51:07 +0200

    added db connection

---
+++ Server/config/db.js
@@ -0,0 +1,29 @@
+// config/db.js
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load .env variables
+
+const DB_CONNECTION = process.env.DB_CONNECTION;
+
+if (!DB_CONNECTION) {
+    console.error("Error: MONGO_URI not defined in .env");
+    process.exit(1);
+}
+
+const connectDB = async () => {
+    try {
+        console.log("Connecting to MongoDB:", process.env.DB_CONNECTION);
+        await mongoose.connect(process.env.DB_CONNECTION, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB Connected");
+    } catch (err) {
+
+        console.error('MongoDB connection error:', err.message);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
