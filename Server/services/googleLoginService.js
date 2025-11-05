777a2fb9658272fef4047b51e045a74026010d26
michal hanau <mh3182100@gmail.com>
2025-11-05 12:21:25 +0200

    Google login - server

--- Server/services/googleLoginService.js
+++ Server/services/googleLoginService.js
@@ -1,15 +1,15 @@
-import usersModel from '../models/usersModel.js';
+// import usersModel from '../models/usersModel.js';
 
-export async function googleLoginSupplier(ticket) {
-    const payload = ticket.getPayload();
-    if (!payload) return { error: "Invalid payload" };
+export async function googleLoginS(ticket) {
+    // const payload = ticket.getPayload();
+    // if (!payload) return { error: "Invalid payload" };
 
-    const { sub: googleId, email, name, picture } = payload;
+    // const { sub: googleId, email, name, picture } = payload;
 
-    const user = await usersModel.findOne({ email });
+    // const user = await usersModel.findOne({ email });
 
-    if (!user) {
-        return { isNewUser: true, payload };
-    }
-    return { isNewUser: false, user };
+    // if (!user) {
+    //     return { isNewUser: true, payload };
+    // }
+    // return { isNewUser: false, user };
 }
