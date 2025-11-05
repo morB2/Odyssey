8bf057d17ee6e595d3d66bd17e283b651d5ae4bc
michal hanau <mh3182100@gmail.com>
2025-11-05 10:17:23 +0200

    Opening and organizing server folders

---
+++ Server/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import http from 'http';
+import cors from 'cors';
+import { routesInit } from './routes/config_routes.js';
+import './db/mongoConect.js';
+import { config } from './config/secret.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+routesInit(app);
+
+const server = http.createServer(app);
+
+const port = config.port || 3000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
