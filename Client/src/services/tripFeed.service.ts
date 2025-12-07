9797c2bdfd7f02dc156630767ed82f68cdf09c7c
morB2 <morb9876@gmail.com>
2025-12-07 13:30:19 +0200

    debug5

--- Client/src/services/tripFeed.service.ts
+++ Client/src/services/tripFeed.service.ts
@@ -1,27 +1,12 @@
-import axios from "axios";
 import api from "./httpService";
 
 const TRIPS_PATH = "/trips";
-
-// export const fetchTrips = async (userId: string, page: number = 1, limit: number = 5) => {
-//     try {
-//         const url = userId ? `${TRIPS_PATH}/${userId}` : TRIPS_PATH;
-//         console.log("url", url);
-//         const res = await api.get(url, {
-//             params: { page, limit }
-//         });
-//         return res.data;
-//     } catch (error) {
-//         console.error("Error fetching trips:", error);
-//         throw error;
-//     }
-// };
 
 export const fetchTrips = async (userId: string, page: number = 1, limit: number = 5) => {
     try {
         const url = userId ? `${TRIPS_PATH}/${userId}` : TRIPS_PATH;
         console.log("url", url);
-        const res = await axios.get(`https://odyssey-dbdn.onrender.com${url}`, {
+        const res = await api.get(url, {
             params: { page, limit }
         });
         return res.data;
